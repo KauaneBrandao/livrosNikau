f99c7185198dc9c05d436357a9fb71fcd2e545cc
collyterra <nicollyemillysouza@gmail.com>
2024-08-12 14:26:26 -0300

    fiz o carrosel '_'

--- livrariaNikau/resources/views/layouts/main.blade.php
+++ livrariaNikau/resources/views/layouts/main.blade.php
@@ -19,14 +19,16 @@
 </head>
 
 <body>
-    <header>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div class="collapse navbar-collapse" id="navbar">
-            <a href="" class="navbar-brand">
-                <img src="/img/Nikauwhite.png" alt="">
-            </a>
-            <ul class="navbar-nav">
-                <li class="nav-item">
+
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"><img src="../img/Nikau.png" alt=""></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
                     <a href="/galeria" class="nav-link">Livros</a>
                 </li>
 
@@ -43,10 +45,10 @@
                 </a>
                 </form>
 
-                <li class="nav-item">
-                    <a href="/login" class="nav-link">Entrar</a>
-                </li>
-                <li class="nav-item">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
                     <a href="/register" class="nav-link">Cadastrar</a>
                 </li>
 
--- livrariaNikau/resources/views/welcome.blade.php
+++ livrariaNikau/resources/views/welcome.blade.php
@@ -3,5 +3,57 @@
 @section('title','Livros Nikau')
 
 @section('content')
-<h1>ALAAAAAAAAAAAAAAAAN</h1>
+
+<body>
+
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"><img src="../img/Nikau.png" alt=""></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div class="carrossel">
+    
+   <input type="radio" name="position" checked />
+  <input type="radio" name="position" />
+  <input type="radio" name="position" />
+  <input type="radio" name="position" />
+  <input type="radio" name="position" />
+  <main id="carousel">
+    <div class="item"></div>
+    <div class="item"></div>
+    <div class="item"></div>
+    <div class="item"></div>
+    <div class="item"></div>
+    <main> 
+</div>
+
+    <div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
 @endsection
