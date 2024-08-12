2828b23fd83d99201405a9701083d594f7740c4b
collyterra <nicollyemillysouza@gmail.com>
2024-08-12 20:19:31 -0300

    começando cards 1

--- livrariaNikau/resources/views/welcome.blade.php
+++ livrariaNikau/resources/views/welcome.blade.php
@@ -29,7 +29,18 @@
   </button>
 </div>
 
-    <div class="card" style="width: 18rem;">
+    <div class="card" style="width: 18rem;" id="exampleModal">
+  <img src="../img/tronodevidro.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Trono de Vidro: A herdeira de fogo</h5>
+    <p class="card-text">R$49,90</p>
+      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+       Saiba Mais
+      </button>
+  </div>
+</div>
+
+<div class="card" style="width: 18rem;">
   <img src="..." class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Card title</h5>
@@ -37,5 +48,39 @@
     <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
 </div>
+
+<div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+
+<!-- modais -->
+
+
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+      A história de Celaena Sardothien, uma assassina a serviço de um rei tirânico, é uma fantasia épica repleta de ação, intriga e cenas de luta inesquecíveis. 
+      No terceiro livro da saga, Herdeira do fogo , Celaena ressurge das cinzas ainda mais forte e letal. Com personagens complexos e reviravoltas de tirar o fôlego, o universo de Trono de vidro 
+      se tornou um classico entre os amantes de fantasia, acumulando mais de um milhão de cópias vendidas e se tornando presença frequente nas listas dos mais vendidos.
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
 
 @endsection
